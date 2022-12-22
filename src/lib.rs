e334c927064b5a5fa50fd9ac367ad44efbe22cd9
Iago López Galeiras <iaguis@gmail.com>
2022-12-22 22:53:54 +0100

    lib: refactor registers

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,24 @@
 use std::thread;
 use std::time;
 
+mod registers;
+
+use registers::Registers;
+
 pub struct Emulator {
     cpu: Cpu,
     counter: u8,
 }
 
 struct Cpu {
-    af: u16,
-    bc: u16,
-    de: u16,
-    hl: u16,
-
-    sp: u16,
-    pc: u16,
-
-    m: u8,
-    t: u8,
+    reg: Registers
 }
 
 impl Emulator {
     pub fn new() -> Emulator {
         Emulator {
             cpu: Cpu {
-                af: 0,
-                bc: 0,
-                de: 0,
-                hl: 0,
-                sp: 0,
-                pc: 0,
-                m: 0,
-                t: 0,
+                reg: Registers::new()
             },
             counter: 0,
         }
@@ -55,7 +43,7 @@ impl Emulator {
         loop {
             println!("emulating...");
 
-            let opcode = self.read_byte(self.cpu.pc);
+            let opcode = self.read_byte(self.cpu.reg.pc);
             self.counter -= self.get_cycles(opcode);
 
             self.execute(opcode);
---
+++ src/registers.rs
@@ -0,0 +1,66 @@
+pub struct Registers {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+    h: u8,
+    l: u8,
+
+    sp: u16,
+    pub pc: u16,
+
+    m: u8,
+    t: u8,
+}
+
+impl Registers {
+    pub fn new() -> Registers {
+        Registers {
+            a: 0,
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            f: 0,
+            h: 0,
+            l: 0,
+            sp: 0,
+            pc: 0,
+            m: 0,
+            t: 0,
+        }
+    }
+
+    pub fn af(&self) -> u16 {
+        ((self.a as u16) << 8) | (self.f as u16)
+    }
+
+    pub fn bc(&self) -> u16 {
+        ((self.b as u16) << 8) | (self.c as u16)
+    }
+
+    pub fn de(&self) -> u16 {
+        ((self.d as u16) << 8) | (self.e as u16)
+    }
+
+    pub fn hl(&self) -> u16 {
+        ((self.h as u16) << 8) | (self.l as u16)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn combine_a_and_f() {
+        let mut mock_registers = Registers::new();
+
+        mock_registers.a = 0x10;
+        mock_registers.f = 0xff;
+
+        assert_eq!(mock_registers.af(), 0x10ff);
+    }
+}
